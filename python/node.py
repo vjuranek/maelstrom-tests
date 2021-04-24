5a3096896934e07e3156fabd27d48529c584dfc9
Vojtech Juranek <vojtech.juranek@gmail.com>
2021-04-24 22:37:15 +0200

    Thread safety

--- python/node.py
+++ python/node.py
@@ -1,18 +1,21 @@
 import json
 import sys
+import threading
 
 
 class Node:
     def __init__(self):
         self.node_id = None
         self.msg_id = 1
+        self._lock = threading.RLock()
 
     def reply(self, dest, body):
-        resp = {
-            "src": self.node_id,
-            "dest": dest,
-            "body": body
-        }
-        # sys.stderr.write(json.dumps(resp) + "\n")
-        sys.stdout.write(json.dumps(resp) + "\n")
-        sys.stdout.flush()
+        with self._lock:
+            resp = {
+                "src": self.node_id,
+                "dest": dest,
+                "body": body
+            }
+            # sys.stderr.write(json.dumps(resp) + "\n")
+            sys.stdout.write(json.dumps(resp) + "\n")
+            sys.stdout.flush()
