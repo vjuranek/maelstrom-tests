ed66af05bf90e54d0067dfaf1ab693c8856544a7
Vojtech Juranek <vojtech.juranek@gmail.com>
2021-06-11 23:07:57 +0200

    Add Maelstrom errors

---
+++ python/errors.py
@@ -0,0 +1,48 @@
+class MaelstromError(Exception):
+    err_no = None
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg.format(self=self)
+
+
+class TimeoutError(MaelstromError):
+    err_no = 0
+
+
+class NotSupportedError(MaelstromError):
+    err_no = 10
+
+
+class TemporarilyUnavailableError(MaelstromError):
+    err_no = 11
+
+
+class MalformedRequestError(MaelstromError):
+    err_no = 12
+
+
+class CrashError(MaelstromError):
+    err_no = 13
+
+
+class AbortError(MaelstromError):
+    err_no = 14
+
+
+class KeyDoesnExistError(MaelstromError):
+    err_no = 20
+
+
+class KeyAlreadyExistsError(MaelstromError):
+    err_no = 21
+
+
+class PreconditionFailedError(MaelstromError):
+    err_no = 22
+
+
+class TxnConflictError(MaelstromError):
+    err_no = 30
