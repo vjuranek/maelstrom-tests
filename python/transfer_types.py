94fd9111b1efa089cb05039d0fb00e14274d747f
Vojtech Juranek <vojtech.juranek@gmail.com>
2021-07-17 22:15:05 +0200

    txn: adjust DbNode to thunks

--- python/transfer_types.py
+++ python/transfer_types.py
@@ -103,39 +103,55 @@ class ServiceRequest:
 
 class DbNode:
 
-    def __init__(self, db={}):
-        self._db = db
+    def __init__(self, node, id_gen, map={}):
+        self.node = node
+        self.id_gen = id_gen
+        self._map = map
 
     def copy(self):
-        return DbNode(self._db.copy())
+        return DbNode(self._map.copy())
 
     def to_json(self):
-        return json.dumps(self._db)
+        db_map = {}
+        for key, thunk in self._map:
+            db_map[key] = thunk.id()
+        return json.dumps(db_map)
 
     @classmethod
-    def from_json(cls, db_json):
-        return DbNode(json.loads(db_json))
+    def from_json(cls, node, id_gen, db_json):
+        pairs = json.loads(db_json)
+        db_map = {}
+        for key, id in pairs:
+            db_map[key] = Thunk(node, id, None, True)
+        return DbNode(node, id_gen, db_map)
+
+    def save(self):
+        for thunk in self._map:
+            thunk.save()
 
     def get(self, key):
-        return self._db.get(key)
+        thunk = self._map.get(key)
+        if thunk:
+            return thunk.value()
 
     def append(self, key, value):
-        new_db = self._db.copy()
-        new_db[key] = value
-        return new_db
+        thunk = Thunk(self.node, self.id_gen.next(), value, False)
+        new_db = self._map.copy()
+        new_db[key] = thunk
+        return DbNode(self.node, self.id_gen, new_db)
 
     def apply_txn(self, txn):
         res = []
         for fn, key, value in txn:
             if fn == "r":
-                res.append([fn, key, self._db.get(str(key))])
+                res.append([fn, key, self._map.get(str(key))])
             elif fn == "append":
                 res.append([fn, key, value])
                 # DB is dict str -> list.
                 key = str(key)
-                value_list = self._db[key].copy() if key in self._db else []
+                value_list = self._map[key].copy() if key in self._map else []
                 value_list.append(value)
-                self._db[key] = value_list
+                self._map[key] = value_list
             else:
                 raise Exception("Unknown TXN operation {!r}".format(fn))
         return [self, res]
@@ -143,7 +159,7 @@ class DbNode:
     def __eq__(self, other):
         if not isinstance(other, DbNode):
             return False
-        return self._db == other._db
+        return self._map == other._map
 
 
 class Thunk:
