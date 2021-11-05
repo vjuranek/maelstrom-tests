8642a799b28491b0021bc06e3b26af3732d20430
Vojtech Juranek <vojtech.juranek@gmail.com>
2021-11-06 00:48:56 +0100

    echo go: use proper go formating

--- go/echo/echo.go
+++ go/echo/echo.go
@@ -8,10 +8,9 @@ import (
 	"encoding/json"
 )
 
-
 func reply(req map[string]interface{}, body map[string]interface{}, nodeId string, msgId int) {
 	resp := map[string]interface{}{
-		"src": nodeId,
+		"src":  nodeId,
 		"dest": req["src"].(string),
 		"body": body,
 	}
@@ -22,9 +21,9 @@ func reply(req map[string]interface{}, body map[string]interface{}, nodeId strin
 func main() {
 	var msgId = 0
 	var nodeId string
-	
+
 	scanner := bufio.NewScanner(os.Stdin)
-	
+
 	for scanner.Scan() {
 		var rawReq = scanner.Text()
 		var req map[string]interface{}
@@ -32,14 +31,14 @@ func main() {
 		var body = req["body"].(map[string]interface{})
 
 		msgId = msgId + 1
-		respBody := map[string]interface{} {	
-			"msg_id": msgId,
+		respBody := map[string]interface{}{
+			"msg_id":      msgId,
 			"in_reply_to": int(body["msg_id"].(float64)),
 		}
-		
+
 		switch reqType := body["type"]; reqType {
 		case "init":
-			nodeId = body["node_id"].(string)			
+			nodeId = body["node_id"].(string)
 			respBody["type"] = "init_ok"
 		case "echo":
 			respBody["type"] = "echo_ok"
